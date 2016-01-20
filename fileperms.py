897fc9e9eda4fb12d88ec2f1f0a09168dd548fd8
Atticuss <jonn.callahan@gmail.com>
2016-01-20 11:47:38 -0500

    now tracks all world perms

--- fileperms.py
+++ fileperms.py
@@ -10,10 +10,21 @@ flist=[]
 for (dirpath,dirname,filenames) in os.walk(sys.argv[1]):
 	flist.extend(['%s/%s'%(dirpath,filename) for filename in filenames])
 
-num = 0
+readable = 0
+writeable = 0
+executable = 0
 for f in flist:
 	if os.stat(f)[ST_MODE] & 4 != 0:
 		print('world readable file: %s'%f)
-		num+=1
+		readable+=1
+	if os.stat(f)[ST_MODE] & 2 != 0:
+		print('world writable file: %s'%f)
+		writable+=1
+	if os.stat(f)[ST_MODE] & 1 != 0:
+		print('world executable file: %s'%f)
+		executable+=1
 
-print('\nTotal files: %s -- World readable files: %s'%(len(flist),nume))
+print('\nTotal files: %s'%len(flist))
+print('World readable: %s'%readable)
+print('World writable: %s'%writeable)
+print('World executable: %s'%executable)
