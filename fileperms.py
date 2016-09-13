91fcbb2588115a037e31a90673b3676e88dd7fde
Ryan Reid <aur3lius.dev@gmail.com>
2016-09-13 16:24:46 -0400

    Re-write of fileperms for easy re-use during copy paste into interpreter.
    Same-same, but different. Also prints out the percent of files with particular file permissions rather the number of files. Full results can be printed via the "print_list" method.

--- fileperms.py
+++ fileperms.py
@@ -1,30 +1,80 @@
+"""
+This script processes a given directory and prints the
+    percentage of world permissions found.
+Re-write of https://github.com/Atticuss/FilePerms
+"""
 import sys
-from stat import *
 import os
+from stat import ST_MODE
+
+
+class Perms:
+    """Contains the methods/function related to parsing and printing
+        file perms."""
+    def __init__(self, input_dir):
+        self.indir = input_dir
+        self.flist = []
+        self.rlist = []
+        self.wlist = []
+        self.elist = []
+
+    def get_files(self):
+        """Builds a list of files for a give directory"""
+        for (dirpath, _, filenames) in os.walk(self.indir):
+            self.flist.extend(['%s/%s' % (dirpath, filename)
+                               for filename in filenames])
+
+    def parse_perms(self):
+        """Parses the files in flist for file perms"""
+        for permf in self.flist:
+            if os.stat(permf)[ST_MODE] & 4 != 0:
+                self.rlist.append(permf)
+            if os.stat(permf)[ST_MODE] & 2 != 0:
+                self.wlist.append(permf)
+            if os.stat(permf)[ST_MODE] & 1 != 0:
+                self.elist.append(permf)
+
+    @staticmethod
+    def _freq(x, y):
+        """Returns the frequency of occurence for 2 numbers"""
+        return x / y * 100.0
+
+    def stats(self):
+        """return %readable, %writable, %executable"""
+        return (self._freq(len(self.rlist), len(self.flist)),
+                self._freq(len(self.wlist), len(self.flist)),
+                self._freq(len(self.elist), len(self.flist)))
+
+    def print_list(self, ltype, pnum=None):
+        """Print list based on ltype and pnum
+            pnum is the number of items to print"""
+        if ltype == "e":
+            if not pnum:
+                pnum = len(self.elist)
+            for i in xrange(0, pnum):
+                print(self.elist[i])
+        elif ltype == 'w':
+            if not pnum:
+                pnum = len(self.wlist)
+            for i in xrange(0, pnum):
+                print(self.wlist[i])
+        elif ltype == 'r':
+            if not pnum:
+                pnum = len(self.rlist)
+            for i in xrange(0, pnum):
+                print(self.rlist[i])
 
 if len(sys.argv) < 2:
-	print('Usage: python fileperms.py <target dir>')
-	sys.exit(1)
+    indir = input("Enter the directory to parse: ")
+else:
+    indir = sys.argv[1]
 
-flist=[]
-for (dirpath,dirname,filenames) in os.walk(sys.argv[1]):
-	flist.extend(['%s/%s'%(dirpath,filename) for filename in filenames])
+p = Perms(indir)
+p.get_files()
+p.parse_perms()
+readable, writable, executable = p.stats()
 
-readable = 0
-writeable = 0
-executable = 0
-for f in flist:
-	if os.stat(f)[ST_MODE] & 4 != 0:
-		print('world readable file: %s'%f)
-		readable+=1
-	if os.stat(f)[ST_MODE] & 2 != 0:
-		print('world writable file: %s'%f)
-		writable+=1
-	if os.stat(f)[ST_MODE] & 1 != 0:
-		print('world executable file: %s'%f)
-		executable+=1
-
-print('\nTotal files: %s'%len(flist))
-print('World readable: %s'%readable)
-print('World writable: %s'%writeable)
-print('World executable: %s'%executable)
+print('\nTotal files: %s' % len(p.flist))
+print('%% +R: %s' % readable)
+print('%% +W: %s' % writable)
+print('%% +E: %s' % executable)
